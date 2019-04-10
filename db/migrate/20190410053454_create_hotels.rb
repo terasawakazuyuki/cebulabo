0c0716b5ee5d6b24dc5f801fef5cc9386297095c
Terasawa Kazuyuki <ie2965@hotmail.com>
2019-04-10 14:43:47 +0900

    Edit hotels table

--- db/migrate/20190410053454_create_hotels.rb
+++ db/migrate/20190410053454_create_hotels.rb
@@ -1,7 +1,12 @@
 class CreateHotels < ActiveRecord::Migration[5.2]
   def change
     create_table :hotels do |t|
-
+        t.string      :hotel_name_ja
+        t.string      :hotel_name_en
+        t.integer     :hotel_rank
+        t.string      :hotel_area
+        t.string      :hotel_image
+        t.string      :hotel_image_text
       t.timestamps
     end
   end
